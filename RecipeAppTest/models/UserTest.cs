1542b196d514fc43808c86a891963b64cb6fcc69
bablushka <119828214+bablushka@users.noreply.github.com>
2024-03-13 11:47:27 -0400

    made unit test for every name exception case

--- RecipeAppTest/models/UserTest.cs
+++ RecipeAppTest/models/UserTest.cs
@@ -18,11 +18,21 @@ public class UserTest {
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
-    public void NameLo_Throws_ArugumentException() {
+    public void NameNotEnoughCharacters_Throws_ArugumentException() {
         //Arrange
         List<Recipe> favorites = new List<Recipe>();
         //Act
-        User Rida = new User(null, "rida was here", "PrabBoss123", favorites);
+        User Rida = new User("", "rida was here", "PrabBoss123", favorites);
+        //Assert
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void NameExceedsCharacters_Throws_ArugumentException() {
+        //Arrange
+        List<Recipe> favorites = new List<Recipe>();
+        //Act
+        User Rida = new User("Riiiiiiiiiiiiida", "rida was here", "PrabBoss123", favorites);
         //Assert
     }
 }
