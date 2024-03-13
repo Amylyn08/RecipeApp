08d52835d08ab3188ada54a2931e4749412f492a
Amy <amynguyen0132@gmail.com>
2024-03-13 11:03:33 -0400

    Some rating tests done, merging for teammates to get rid of errors

--- RecipeApp/models/Rating.cs
+++ RecipeApp/models/Rating.cs
@@ -1,16 +1,27 @@
 using System.Dynamic;
 
 namespace RecipeApp.Models;
+using RecipeApp.Constants;
 
+/// <summary>
+/// For rating of recipes, contains stars, description, and user/author of the rating.
+/// </summary>
 public class Rating {
      public int __stars{get; private set;}
      public string __description{get; private set;}
      public User __user{get; private set;}
 
+/// <summary>
+/// Constructor of Rating.
+/// </summary>
+/// <param name="stars">Stars for rating out of 5</param>
+/// <param name="desc">Dscription of the rating</param>
+/// <param name="user">User who created the rating</param>
+/// <exception cref="ArgumentException">Throws exceptions if user disrespects contraints</exception>
     public Rating(int stars, string desc, User user){
         if (stars > 5 || stars < 0) throw new ArgumentException("Stars must be between 0 to 5");
         if(user == null) throw new ArgumentException("User cannot be null");
-        if(desc.Length > 512) throw new ArgumentException("Max description length exceeded!");
+        if(desc.Length > Constants.MAX_DESCRIPTION_LENGTH) throw new ArgumentException("Max description length exceeded!");
         if(desc == null) desc = "";
 
         __stars = stars;
