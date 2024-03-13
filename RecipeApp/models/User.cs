ef15440969406aa9292c140b9bdc32ed5633e5bc
bablushka <119828214+bablushka@users.noreply.github.com>
2024-03-13 09:48:11 -0400

    started User class - made the fields and started the constructor

--- RecipeApp/models/User.cs
+++ RecipeApp/models/User.cs
@@ -1,5 +1,21 @@
 namespace RecipeApp.Models;
-
+/// <summary>
+/// Represents a user
+/// </summary>
 public class User {
+    public string Name { get; private set;}
+    //Profile pic --> will implement when teacher shows us
+    public string Description {get; private set;}
+    public string Password;
+    public List<Recipe> Favorites{get; private set;}
 
+    public User(string name, string desc, string pass, List<Recipe> favorites) {
+        if(name == null) throw new ArgumentException("Name cannot be null!");
+        if(name.Length == 0) throw new ArgumentException("Name cannot be empty!");
+
+        this.Name = name;
+        this.Description = desc;
+        this.Password = pass;
+        this.Favorites = favorites;
+    }
 }
