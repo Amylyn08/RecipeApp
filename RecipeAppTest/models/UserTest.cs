02c4a36044ff042638c67396f0010a1b13e645c0
bablushka <119828214+bablushka@users.noreply.github.com>
2024-03-13 11:53:35 -0400

    made unit test for every password exception case

--- RecipeAppTest/models/UserTest.cs
+++ RecipeAppTest/models/UserTest.cs
@@ -35,4 +35,24 @@ public class UserTest {
         User Rida = new User("Riiiiiiiiiiiiida", "rida was here", "PrabBoss123", favorites);
         //Assert
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void NullPassword_Throws_ArugumentException() {
+        //Arrange
+        List<Recipe> favorites = new List<Recipe>();
+        //Act
+        User Rida = new User("Rida", "rida was here", null, favorites);
+        //Assert
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void PasswordNotEnoughCharacters_Throws_ArugumentException() {
+        //Arrange
+        List<Recipe> favorites = new List<Recipe>();
+        //Act
+        User Rida = new User("Rida", "rida was here", "Prab", favorites);
+        //Assert
+    }
 }
