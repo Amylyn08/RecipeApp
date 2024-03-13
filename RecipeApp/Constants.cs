a790856781a9f14dd5fc8a47f4ff9ad48f17a272
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-03-13 10:39:40 -0400

    Created Recipe, Ingredient and UnitOfMeasurement

---
+++ RecipeApp/Constants.cs
@@ -0,0 +1,5 @@
+namespace RecipeApp.Constants;
+
+public class Constants {
+    public static int MAX_DESCRIPTION_LENGTH = 256;
+}
---
+++ RecipeApp/models/Ingredient.cs
@@ -0,0 +1,29 @@
+namespace RecipeApp.Models;
+
+/// <summary>
+/// An ingredient inside a recipe
+/// </summary>
+public class Ingredient {
+    public string Name { get; private set; }
+    public int Quantity { get; private set; }
+    public UnitOfMeasurement UnitOfMeasurement { get; private set; }
+    public int Price { get; private set; }
+    
+    /// <summary>
+    /// Constructor with name, quantity and unit of measurement
+    /// </summary>
+    /// <param name="name">Name like "tomato" or "potato"</param>
+    /// <param name="quantity">Quantity like 0.5 or 1</param>
+    /// <param name="unitOfMeasurement">Grams, teaspoons, etc</param>
+    /// <exception cref="ArgumentException">If name is null, empty or if quantity is <= 0</exception>
+    public Ingredient(string name, int quantity, UnitOfMeasurement unitOfMeasurement, int price) {
+        if (name == null) throw new ArgumentException("Name cannot be null");
+        if (name.Length == 0) throw new ArgumentException("Name cannot be empty");
+        if (quantity <= 0) throw new ArgumentException("Quantity must be greater than 0");
+        if (price < 0) throw new ArgumentException("Price cannot be negative");
+        this.Name = name;
+        this.Quantity = quantity;
+        this.UnitOfMeasurement = unitOfMeasurement;
+        this.Price = price;
+    }
+}
