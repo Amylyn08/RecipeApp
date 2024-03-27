00b309a7a54f1642b30f1e4b3946e0116b7671e5
bablushka <119828214+bablushka@users.noreply.github.com>
2024-03-27 10:50:04 -0400

    unit test null password on login

--- RecipeAppTest/services/UserServiceTest.cs
+++ RecipeAppTest/services/UserServiceTest.cs
@@ -13,8 +13,16 @@ public class UserServiceTest {
         string password = "RidaPassword";
 
         us.Login(username,password);
+    }
 
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void NullPassword_Login_ThrowsException() {
+        UserService us = new UserService();
+        string username = "Rida1";
+        string password = null;
 
+        us.Login(username,password);
     }
 
 }
