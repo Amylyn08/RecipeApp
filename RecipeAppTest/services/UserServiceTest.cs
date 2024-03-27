9e56bf6f033c596cc84d71ba20cd2a628666517c
bablushka <119828214+bablushka@users.noreply.github.com>
2024-03-27 10:48:33 -0400

    unit test null username on login

--- RecipeAppTest/services/UserServiceTest.cs
+++ RecipeAppTest/services/UserServiceTest.cs
@@ -1,6 +1,20 @@
+using RecipeApp.Services;
+
 namespace RecipeAppTest.Services;
 
 [TestClass]
 public class UserServiceTest {
-    
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void NullUsername_Login_ThrowsException() {
+        UserService us = new UserService();
+        string username = null;
+        string password = "RidaPassword";
+
+        us.Login(username,password);
+
+
+    }
+
 }
