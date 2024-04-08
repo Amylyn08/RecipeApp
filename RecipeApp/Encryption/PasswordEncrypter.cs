28a74b61c428b2dc43aded7a2696862a4cadfc9c
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-08 10:21:38 -0400

    Added interface for password encrytion

--- RecipeApp/Encryption/PasswordEncrypter.cs
+++ RecipeApp/Encryption/PasswordEncrypter.cs
@@ -1,2 +1,9 @@
 namespace RecipeApp.Security;
 
+public class PasswordEncrypter : IEncrypter
+{
+    public string Encrypt(string plainText)
+    {
+        throw new NotImplementedException();
+    }
+}
