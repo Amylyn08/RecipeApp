fed50da6afc911314922c92678cf64de9c428ec5
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-10 12:38:13 -0400

    Refactored Step

--- RecipeApp/models/Step.cs
+++ RecipeApp/models/Step.cs
@@ -7,24 +7,31 @@ namespace RecipeApp.Models;
 /// </summary>
 /// [Ke]
 /// 
-[Keyless]
 public class Step {
     private int _timeInMinutes;
     private string _instruction;
 
-    public Step() {}
+    public int StepId {
+        get; 
+        set;
+    }
 
     public int TimeInMinutes { 
         get => _timeInMinutes; 
         set {
-            CheckTime(value);
+            if (value < 0) 
+                throw new ArgumentException("Time in minutes cannot be negative");
             _timeInMinutes = value;
         } 
     }
+
     public string Instruction { 
         get => _instruction; 
         set {
-            CheckInstruction(value);
+            if (value == null) 
+                throw new ArgumentException("Instruction cannot be null");
+            if (value.Length == 0) 
+                throw new ArgumentException("Instruction cannot be empty");
             _instruction = value;
         } 
     }
@@ -35,35 +42,15 @@ public class Step {
     /// <param name="timeInMinutes">Time to complete step</param>
     /// <param name="instruction">The actual step eg: Do potato</param>
     public Step(int timeInMinutes, string instruction) {
-        CheckTime(timeInMinutes);
-        CheckInstruction(instruction);
-        _timeInMinutes = timeInMinutes;
-        _instruction = instruction;
+        TimeInMinutes = timeInMinutes;
+        Instruction = instruction;
     }
 
     /// <summary>
-    /// Validate the time 
+    /// Overriden ToString()
     /// </summary>
-    /// <param name="timeInMinutes">Time in minutes</param>
-    /// <exception cref="ArgumentException">If negative</exception>
-    private static void CheckTime(int timeInMinutes) {
-        if (timeInMinutes < 0) 
-            throw new ArgumentException("Time in minutes cannot be negative");
-    }
-
-    /// <summary>
-    /// Checks the instruction
-    /// </summary>
-    /// <param name="instruction">Instruction of step</param>
-    /// <exception cref="ArgumentException">If null or empty</exception>
-    private static void CheckInstruction(string instruction) {
-        if (instruction == null) 
-            throw new ArgumentException("Instruction cannot be null");
-        if (instruction.Length == 0) 
-            throw new ArgumentException("Instruction cannot be empty");
-    }
-
+    /// <returns>String representation of a step</returns>
     public override string ToString() {
-        return "Time: " + TimeInMinutes + ", " + Instruction;
+        return "Time: " + TimeInMinutes + "minutes, Instruction: " + Instruction;
     }
 }
