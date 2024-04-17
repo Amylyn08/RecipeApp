032e881fa6615feacb4f451c81ef5a0936669a1b
Amy Nguyen <thuc.nguyen1@dawsoncollege.qc.ca>
2024-04-17 11:16:25 -0400

    Made search by Ingredients

--- RecipeApp/Searcher/SearchByIngredients.cs
+++ RecipeApp/Searcher/SearchByIngredients.cs
@@ -3,7 +3,8 @@ namespace RecipeApp.Searcher;
 using RecipeApp.Context;
 using RecipeApp.Models;
 
-public class SearchByIngredients : SearcherBase{
+public class SearchByIngredients : SearcherBase
+{
 
     private readonly string _criteria;
 
@@ -11,7 +12,8 @@ public class SearchByIngredients : SearcherBase{
     /// Constructor contains the criteria, corresponding to the crtieria specified.
     /// </summary>
     /// <param name="ingredientName">Ingredient name  </param>
-    public SearchByIngredients(string ingredientName) { 
+    public SearchByIngredients(string ingredientName)
+    {
         if (ingredientName == null)
             throw new ArgumentException("Ingredient name cannot be null");
         if (ingredientName.Length == 0)
@@ -19,7 +21,18 @@ public class SearchByIngredients : SearcherBase{
         _criteria = ingredientName;
     }
 
-    public override List<Recipe> FilterRecipes() {
-        throw new NotImplementedException();
+
+    /// <summary>
+    /// Searches list of recipes of context and returns those that includes the name.
+    /// Using LINQ quering
+    /// </summary>
+    /// <returns>List of filtered recipes</returns>
+    public override List<Recipe> FilterRecipes()
+    {
+        List<Recipe> recipeList = (from recipe in Context.Recipes
+                                   where recipe.Ingredients.Any(ing => ing.Name.Contains(_criteria))
+                                   select recipe).ToList<Recipe>();
+        return recipeList;
+
     }
 }
