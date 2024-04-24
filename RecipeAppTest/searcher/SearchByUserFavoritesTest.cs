b1ae6eadc9ce1cbd918e23333c6a44b4391f1394
Amy Nguyen <Amynguyen0132@gmail.com>
2024-04-23 22:03:15 -0400

    Finished search by user favorite, modified favorutie class to have constructor and equals method, andn proper setters

--- RecipeAppTest/searcher/SearchByUserFavoritesTest.cs
+++ RecipeAppTest/searcher/SearchByUserFavoritesTest.cs
@@ -11,74 +11,69 @@ public class SearchByUserFavoritesTest
 
     [TestMethod]
     public void SearchByFavsReturnsCorrectList()
-    {
-        // //Arrange
-        // List<Ingredient> ings1 = new() {
-        //     new Ingredient("caramel", 5, UnitOfMeasurement.AMOUNT, 20.2)
-        // };
+    {   
+        
+        List<Ingredient> ings1 = new() {
+            new Ingredient("caramel", 5, UnitOfMeasurement.AMOUNT, 20.2)
+        };
+        List<Ingredient> ings2 = new() {
+            new Ingredient("apples", 5, UnitOfMeasurement.AMOUNT, 20.2)
+        };
+        List<Ingredient> ings3 = new() {
+            new Ingredient("potato", 5, UnitOfMeasurement.AMOUNT, 20.2)
+        };
+        List<Ingredient> ings4 = new() {
+            new Ingredient("bread", 5, UnitOfMeasurement.AMOUNT, 20.2)
+        };
 
-        // List<Step> steps = new() {
-        //     new Step(10, "eat")
-        // };
-        // User user = new User("Rida2", "I am rida 2", "RidaPassword", new(), new(), "randomsalt");
+        List<Step> steps = new() {
+            new Step(10, "eat")
+        };
 
-        // List<Rating> ratings0 = new List<Rating> {
-        // new(4, "amazing", new()),
-        // new(5, "fantastic", new())
-        // };
+        User user = new User("Rida2", "I am rida 2", "RidaPassword", new(), new(), "randomsalt");
 
-        // List<Recipe> favList1 = new List<Recipe>{
-        //     new ("Salad", user, "desc", 2, ings1, steps, ratings0, new()),
-        //     new ("BTL", user, "desc", 1, ings1, steps, ratings0, new()),
-        //     new ("Paneer", user, "desc", 1, ings1, steps, ratings0, new()),
-        // };
-        // List<Recipe> favList2 = new List<Recipe>{
-        //     new ("Hamburger", user, "desc", 2, ings1, steps, ratings0, new()),
-        //     new ("Sharwma", user, "desc", 1, ings1, steps, ratings0, new()),
-        // };
-        // List<Recipe> favList3 = new List<Recipe>{
-        //     new ("Shish Taouk", user, "desc", 2, ings1, steps, ratings0, new()),
-        //     new ("Chick Pea Salad", user, "desc", 1, ings1, steps, ratings0, new()),
-        // };
-        // List<Recipe> favList4 = new List<Recipe>{
-        //     new ("Tofu Stir Fry", user, "desc", 2, ings1, steps, ratings0, new()),
-        //     new ("Lo Mein", user, "desc", 1, ings1, steps, ratings0, new()),
-        // };
-        // List<Recipe> favList5 = new List<Recipe>{
-        //     new ("Wonton Soup", user, "desc", 2, ings1, steps, ratings0, new()),
-        //     new ("Xiu mai", user, "desc", 1, ings1, steps, ratings0, new()),
-        // };
+        List<Rating> ratings0 = new List<Rating> {
+            new(4, "amazing", new()),
+            new(5, "fantastic", new())
+        };
 
-        // var listData = new List<User>
-        // {
-        //     new("Amy", "student", "AmyStudent", favList1, new(), "meow"),
-        //     new("Jeffrey", "student", "AmyStudent", favList2, new(), "meow"),
-        //     new("Michael", "student", "AmyStudent", favList3, new(), "meow"),
-        //     new("Prabhjot", "student", "AmyStudent", favList4, new(), "meow"),
-        //     new("Rida", "student", "AmyStudent", favList5, new(), "meow")
-        // };
+        Recipe recipe = new Recipe("Recipe0", user, "desc", 2, ings1, steps, ratings0, new());
+        Recipe recipe1 = new Recipe("Recipe1", user, "desc", 2, ings1, steps, ratings0, new());
+        Recipe recipe2 = new Recipe("Recipe2", user, "desc", 2, ings1, steps, ratings0, new());
+        Recipe recipe3 = new Recipe("Recipe3", user, "desc", 2, ings1, steps, ratings0, new());
+        Recipe recipe4 = new Recipe("Recipe4", user, "desc", 2, ings1, steps, ratings0, new());
 
-        // User amy = new("Amy", "student", "AmyStudent", favList1, favList1, "hi");
+        User user1 = new("Amy", "human", "amyhuman123", new(), new(), "meow");
+        User user2 = new("Prabhjot", "human", "amyhuman123", new(), new(), "meow");
+        User user3 = new("Rida", "human", "amyhuman123", new(), new(), "meow");
+        var listData = new List<Favourite>(){
+            new(recipe, user1),
+            new(recipe1, user1),
+            new(recipe2, user1),
+            new(recipe3, user2),
+            new(recipe4, user3),
 
-        // var data = listData.AsQueryable();
+        };
+        //Arrange
+        var data = listData.AsQueryable();
 
-        // var mockSet = new Mock<DbSet<Recipe>>();
-        // mockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(data.Provider);
-        // mockSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(data.Expression);
-        // mockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(data.ElementType);
-        // mockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+        var mockSet = new Mock<DbSet<Favourite>>();
+        mockSet.As<IQueryable<Favourite>>().Setup(m => m.Provider).Returns(data.Provider);
+        mockSet.As<IQueryable<Favourite>>().Setup(m => m.Expression).Returns(data.Expression);
+        mockSet.As<IQueryable<Favourite>>().Setup(m => m.ElementType).Returns(data.ElementType);
+        mockSet.As<IQueryable<Favourite>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
-        // var mockContext = new Mock<SplankContext>();
-        // mockContext.Setup(m => m.Recipes).Returns(mockSet.Object);
-        // SearchByUserFavorite searcher = new(mockContext.Object, amy);
+        var mockContext = new Mock<SplankContext>();
+        mockContext.Setup(m => m.Favourites).Returns(mockSet.Object);
+        SearchByUserFavorite searcher = new(mockContext.Object, user1);
 
-        // //Act
-        // List<Recipe> filteredList = searcher.FilterRecipes();
+        //Act
+        List<Recipe> filteredList = searcher.FilterRecipes();
 
-        // //Assert
-        // Assert.AreEqual(3, filteredList.Count);
-        // Assert.AreEqual("Salad", filteredList[0].Name);
-        // Assert.AreEqual("BLT", filteredList[1].Name);
-        // Assert.AreEqual("Paneer", filteredList[2].Name);
+        //Asert
+        Assert.AreEqual(3, filteredList.Count);
+        Assert.AreEqual("Recipe0", filteredList[0].Name);
+        Assert.AreEqual("Recipe1", filteredList[1].Name);
+        Assert.AreEqual("Recipe2", filteredList[2].Name);
     }
 }
