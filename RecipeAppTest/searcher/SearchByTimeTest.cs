da94fc7200d3041a27ef0c40fe06ac13e96d226e
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-04-27 02:30:10 -0400

    All searcher bugs are fixed and all unit tests now accomodate the new queries

--- RecipeAppTest/searcher/SearchByTimeTest.cs
+++ RecipeAppTest/searcher/SearchByTimeTest.cs
@@ -62,48 +62,51 @@ public class SearchByTimeTest {
             new Ingredient("bread", 5, UnitOfMeasurement.AMOUNT, 20.2)
         };
 
-        List<Step> steps = new() {
-            new Step(5, "eat")
-        };
-        List<Step> steps1 = new() {
-            new Step(10, "eat")
-        };
-        List<Step> steps2 = new() {
-            new Step(30, "eat")
-        };
-        List<Step> steps3 = new() {
-            new Step(25, "eat")
-        };
-        List<Step> steps4 = new() {
-            new Step(75, "eat")
-        };
+        User user = new("Rida2", "I am rida 2", "RidaPassword", new(), new(), "randomsalt");
 
-        User user = new User("Rida2", "I am rida 2", "RidaPassword", new(), new(), "randomsalt");
-
-        List<Rating> ratings0 = new List<Rating> {
+        List<Rating> ratings0 = new()
+        {
             new(4, "amazing", new()),
             new(5, "fantastic", new())
         };
 
+        var listStep = new List<Step>() 
+        {
+            new(5, "eat") { RecipeId = 1 },
+            new(10, "eat") { RecipeId = 2 },
+            new(30, "eat") { RecipeId = 3 },
+            new(25, "eat") { RecipeId = 4 },
+            new(75, "eat") { RecipeId = 5 },
+        };
+
         var listData = new List<Recipe>
         {
-            new ("Recipe0", user, "desc", 2, ings1, steps, ratings0, new()),
-            new ("Recipe1", user, "desc", 1, ings4, steps1, ratings0, new()),
-            new ("Recipe2", user, "desc", 6, ings3, steps2, ratings0, new()),
-            new ("Recipe3", user, "desc", 2, ings2, steps3, ratings0, new()),
-            new ("Recipe4", user, "desc", 2, ings1, steps4, ratings0, new())
+            new ("Recipe0", user, "desc", 2, ings1, new() { listStep[0] }, ratings0, new())  { RecipeId = 1 },
+            new ("Recipe1", user, "desc", 1, ings4, new() { listStep[1] }, ratings0, new()) { RecipeId = 2 },
+            new ("Recipe2", user, "desc", 6, ings3, new() { listStep[2] }, ratings0, new()) { RecipeId = 3 },
+            new ("Recipe3", user, "desc", 2, ings2, new() { listStep[3] }, ratings0, new()) { RecipeId = 4 },
+            new ("Recipe4", user, "desc", 2, ings1, new() { listStep[4] }, ratings0, new()) { RecipeId = 5 }
         };
 
         var data = listData.AsQueryable();
+        var stepData = listStep.AsQueryable();
 
         var mockSet = new Mock<DbSet<Recipe>>();
+        var stepMockSet = new Mock<DbSet<Step>>();
+
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.Provider).Returns(data.Provider);
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.Expression).Returns(data.Expression);
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.ElementType).Returns(data.ElementType);
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.Provider).Returns(stepData.Provider);
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.Expression).Returns(stepData.Expression);
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.ElementType).Returns(stepData.ElementType);
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.GetEnumerator()).Returns(stepData.GetEnumerator());
+
         var mockContext = new Mock<SplankContext>();
         mockContext.Setup(m => m.Recipes).Returns(mockSet.Object);
+        mockContext.Setup(m => m.Steps).Returns(stepMockSet.Object);
         SearchByTime searcher = new(mockContext.Object, 5, 10);
 
         //Act
@@ -117,7 +120,6 @@ public class SearchByTimeTest {
 
     [TestMethod]
     public void SearchByTimeReturnsEmptyList(){
-         //Arrange
         List<Ingredient> ings1 = new() {
             new Ingredient("caramel", 5, UnitOfMeasurement.AMOUNT, 20.2)
         };
@@ -131,49 +133,52 @@ public class SearchByTimeTest {
             new Ingredient("bread", 5, UnitOfMeasurement.AMOUNT, 20.2)
         };
 
-        List<Step> steps = new() {
-            new Step(5, "eat")
-        };
-        List<Step> steps1 = new() {
-            new Step(10, "eat")
-        };
-        List<Step> steps2 = new() {
-            new Step(30, "eat")
-        };
-        List<Step> steps3 = new() {
-            new Step(25, "eat")
-        };
-        List<Step> steps4 = new() {
-            new Step(75, "eat")
-        };
+        User user = new("Rida2", "I am rida 2", "RidaPassword", new(), new(), "randomsalt");
 
-        User user = new User("Rida2", "I am rida 2", "RidaPassword", new(), new(), "randomsalt");
-
-        List<Rating> ratings0 = new List<Rating> {
+        List<Rating> ratings0 = new()
+        {
             new(4, "amazing", new()),
             new(5, "fantastic", new())
         };
 
+        var listStep = new List<Step>() 
+        {
+            new(5, "eat") { RecipeId = 1 },
+            new(10, "eat") { RecipeId = 2 },
+            new(30, "eat") { RecipeId = 3 },
+            new(25, "eat") { RecipeId = 4 },
+            new(75, "eat") { RecipeId = 5 },
+        };
+
         var listData = new List<Recipe>
         {
-            new ("Recipe0", user, "desc", 2, ings1, steps, ratings0, new()),
-            new ("Recipe1", user, "desc", 1, ings4, steps1, ratings0, new()),
-            new ("Recipe2", user, "desc", 6, ings3, steps2, ratings0, new()),
-            new ("Recipe3", user, "desc", 2, ings2, steps3, ratings0, new()),
-            new ("Recipe4", user, "desc", 2, ings1, steps4, ratings0, new())
+            new ("Recipe0", user, "desc", 2, ings1, new() { listStep[0] }, ratings0, new())  { RecipeId = 1 },
+            new ("Recipe1", user, "desc", 1, ings4, new() { listStep[1] }, ratings0, new()) { RecipeId = 2 },
+            new ("Recipe2", user, "desc", 6, ings3, new() { listStep[2] }, ratings0, new()) { RecipeId = 3 },
+            new ("Recipe3", user, "desc", 2, ings2, new() { listStep[3] }, ratings0, new()) { RecipeId = 4 },
+            new ("Recipe4", user, "desc", 2, ings1, new() { listStep[4] }, ratings0, new()) { RecipeId = 5 }
         };
 
         var data = listData.AsQueryable();
+        var stepData = listStep.AsQueryable();
 
         var mockSet = new Mock<DbSet<Recipe>>();
+        var stepMockSet = new Mock<DbSet<Step>>();
+
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.Provider).Returns(data.Provider);
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.Expression).Returns(data.Expression);
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.ElementType).Returns(data.ElementType);
         mockSet.As<IQueryable<Recipe>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.Provider).Returns(stepData.Provider);
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.Expression).Returns(stepData.Expression);
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.ElementType).Returns(stepData.ElementType);
+        stepMockSet.As<IQueryable<Step>>().Setup(m => m.GetEnumerator()).Returns(stepData.GetEnumerator());
+
         var mockContext = new Mock<SplankContext>();
         mockContext.Setup(m => m.Recipes).Returns(mockSet.Object);
-        SearchByTime searcher = new(mockContext.Object, 0, 4);
+        mockContext.Setup(m => m.Steps).Returns(stepMockSet.Object);
+        SearchByTime searcher = new(mockContext.Object, 1, 4);
 
         //Act
         List<Recipe> filteredList = searcher.FilterRecipes();
