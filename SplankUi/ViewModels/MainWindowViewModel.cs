e0f92c2869d149e7d848153f2dd3aa73fdc371c6
bablushka <119828214+bablushka@users.noreply.github.com>
2024-04-29 10:03:15 -0400

    pushing for prab

--- SplankUi/ViewModels/MainWindowViewModel.cs
+++ SplankUi/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,27 @@
 ﻿using RecipeApp.Services;
 using RecipeApp.Security;
 using RecipeApp.Context;
+using ReactiveUI;
 namespace SplankUi.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private ViewModelBase _contentViewModel;
+    public string Greeting => "FUCK YOU!";
     public MainWindowViewModel() {
         LOR = new LoginViewAndRegisterViewModel();
+        // _contentViewModel = LOR;
     }
 
+
     public LoginViewAndRegisterViewModel LOR {get;}
+
+    public ViewModelBase ContentViewModel {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public void Authentication() {
+        ContentViewModel = new LoginViewAndRegisterViewModel();
+    }
 }
