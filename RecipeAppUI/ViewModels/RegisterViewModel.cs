e8eaaedc8f4aea3df37d11773f77c9e788afe842
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-05-01 11:20:48 -0400

    Added error handling for register

--- RecipeAppUI/ViewModels/RegisterViewModel.cs
+++ RecipeAppUI/ViewModels/RegisterViewModel.cs
@@ -1,21 +1,41 @@
 using ReactiveUI;
 using RecipeApp.Context;
+using RecipeApp.Exceptions;
 using RecipeApp.Services;
+using System;
 
 namespace RecipeAppUI.ViewModels;
 
 public class RegisterViewModel : ViewModelBase {
     private string _username;
     private string _password;
+    private string _description;
     private string _registerErrorMessage;
     private UserService _userService;
 
-    public string Username { get => _username; private set => this.RaiseAndSetIfChanged(ref _username, value); }
-    public string Password { get => _password; private set => this.RaiseAndSetIfChanged(ref _password, value); }
-    public string RegisterErrorMessage { get => _registerErrorMessage; private set => _registerErrorMessage = value; }
+    public string Username { get => _username; set => this.RaiseAndSetIfChanged(ref _username, value); }
+    public string Password { get => _password; set => this.RaiseAndSetIfChanged(ref _password, value); }
+    public string Description { get => _description; set => this.RaiseAndSetIfChanged(ref _description, value); }
+    public string RegisterErrorMessage { get => _registerErrorMessage; set => this.RaiseAndSetIfChanged(ref _registerErrorMessage, value); }
     public UserService UserService { get => _userService; private set => _userService = value; }
 
     public RegisterViewModel(SplankContext context) {
         UserService = new(context, new());
+    }
+
+    public void Register()
+    {
+        try
+        {
+            UserService.Register(Username, Password, Description);
+        }
+        catch (UserAlreadyExistsException e)
+        {
+            RegisterErrorMessage = e.Message;
+        }
+        catch (ArgumentException e)
+        {
+            RegisterErrorMessage = e.Message;
+        }
     }
 }
