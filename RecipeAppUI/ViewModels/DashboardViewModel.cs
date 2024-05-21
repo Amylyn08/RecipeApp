a5ffd15f1e66040b627a7d07bc77f325115e6e3c
Amy Nguyen <Amynguyen0132@gmail.com>
2024-05-20 23:02:38 -0400

    Dashboard edits

--- RecipeAppUI/ViewModels/DashboardViewModel.cs
+++ RecipeAppUI/ViewModels/DashboardViewModel.cs
@@ -208,5 +208,10 @@ namespace RecipeAppUI.ViewModels
                 ErrorMessage = "Error: Unable to go to specific view.";
             }
         }
+
+        public double GetRatingAvgForSingleRecipe(int recipeId){
+            Recipe recipe = Recipes.FirstOrDefault(r => r.RecipeId == recipeId)!;
+            return recipe.GetTotalAverageRating();
+        }
     }
 }
